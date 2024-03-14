1c8741372dd1573905ab84e24723c518ffa720fc
Shatalov.AG <mrstep93@ya.ru>
2024-03-15 00:19:40 +0300

    start

---
+++ Module16.UserTicketService.Tests/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Module16.UserTicketService.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(100 == 100);
+        }
+    }
+}
